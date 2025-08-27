d6cf5740a31a2c361e18a7bcdd9d07f64b315cf3
kysonhensley <hensleykyson@gmail.com>
2025-08-27 08:47:38 -0600

    Add project files.

---
+++ ConsoleFun/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace ConsoleFun
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write the first line!");
+            Console.WriteLine("Write the Second line!");
+            Console.WriteLine("Write the Third line!");
+            Console.WriteLine("Write the Fourth line!");
+            Console.WriteLine("Write the Fifth line!");
+            Console.WriteLine("Whew!... I'm tired now");
+
+            Console.Beep();
+
+            Console.Read();
+        }
+    }
+}
